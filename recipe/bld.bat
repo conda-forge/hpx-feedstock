40cf69066fbd475781996981b594306b9d6f150d
Kor de Jong <k.dejong1@uu.nl>
2024-03-28 10:59:41 +0100

    Fix typo, pass in path to Python, refactor

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,19 +1,20 @@
 mkdir build
 cd build
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
 
 cmake ^
-    ${CMAKE_ARGS} ^
+    %CMAKE_ARGS% ^
+    -D PYTHON_EXECUTABLE="%PYTHON%" ^
     -D CMAKE_INSTALL_LIBDIR=lib ^
     -D HPX_WITH_EXAMPLES=FALSE ^
     -D HPX_WITH_MALLOC="mimalloc" ^
     -D HPX_WITH_NETWORKING=FALSE ^
     -D HPX_WITH_TESTS=FALSE ^
     ..
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
 
 cmake --build . --config Release --parallel %CPU_COUNT%
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
 
 cmake --install .
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,13 +1,14 @@
 pushd test
 
-cmake . \
-    ${CMAKE_ARGS}
-if errorlevel 1 exit 1
+cmake ^
+    %CMAKE_ARGS% ^
+    .
+if errorlevel 1 exit /b 1
 
 cmake --build . --config Release
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
 
 Release\hello_hpx.exe
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
 
 popd
