0a9fe3ca362ff6887d2fdd53d8d03281d5e5aa66
Kor de Jong <k.dejong1@uu.nl>
2024-03-28 12:01:30 +0100

    Refactor

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,13 +1,13 @@
 set -e
 
 
+pushd test
+
 if [[ "$target_platform" == "osx-64" ]]; then
     # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
-
-cd test
-cmake . -G "Ninja" -D CMAKE_BUILD_TYPE="Release"
+cmake -G "Ninja" ${CMAKE_ARGS} .
 cmake --build . --config Release
 ./hello_hpx
