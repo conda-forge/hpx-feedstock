0eb3e7cd814afbbc68c86a39ad9f631080f01003
Oliver Schmitz <o.schmitz@uu.nl>
2025-09-10 11:55:36 +0200

    Python runtime dependency removed

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,7 +13,7 @@ cmake \
     -G"Ninja" \
     ${CMAKE_ARGS} \
     -D CMAKE_INSTALL_LIBDIR=lib \
-    -D Python_EXECUTABLE="$PYTHON" \
+    -D Python_EXECUTABLE="python" \
     -D HPX_WITH_EXAMPLES=FALSE \
     -D HPX_WITH_MALLOC="${malloc:-tcmalloc}" \
     -D HPX_WITH_NETWORKING=FALSE \
