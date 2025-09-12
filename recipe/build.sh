195ec818ad3a8e6bd6d3f8b6185b870d0c7c9fa8
Oliver Schmitz <o.schmitz@uu.nl>
2025-09-12 15:18:01 +0200

    Fix for macOS tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -e
 mkdir build
 pushd build
 
-if [[ "$target_platform" == "osx-64" ]]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
     # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
